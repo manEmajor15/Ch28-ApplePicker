b23ff91ce2f5ac3ada749a301c3830c2ee8eb8e2
manEmajor15 <55005736+manEmajor15@users.noreply.github.com>
2019-09-27 17:28:06 -0400

    Comitting up until GUI implementation

---
+++ Assets/Scripts/Apple.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+
+    public static float bottomY = -20f;
+    int count; //counter since position tracking isn't working
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count++;
+        if (count > 600)  // Keep getting error that the object is already destroyed so no apples spawn
+         {
+             Destroy(this.gameObject);
+         }
+    }
+}
